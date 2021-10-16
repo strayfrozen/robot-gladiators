1cbde8631e10db36607012a82bf1224a3abea1a6
Stray <strayfrozen@yahoo.com>
2021-10-16 16:26:37 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,8 +69,13 @@ var fight = function(enemyName) {
       }
     }
   };
-
-for(var i = 0; i < enemyNames.length; i++) {
+  // function starts a new game
+var startGame = function(){
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+  for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
@@ -89,7 +94,32 @@ for(var i = 0; i < enemyNames.length; i++) {
       } else {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
-      }  
-      
+      }   
     }
+    endGame();
+  };
+
+  // function to end the entire game
+var endGame = function() {
+    // if player is still alive, player wins!
+    if (playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } 
+    else {
+      window.alert("You've lost your robot in battle.");
+    }
+    // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+};
+
+  // start the game when the page loads
+startGame();
     
